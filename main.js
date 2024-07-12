2a9c43fc09b29d262bc8fb650bab960b90c967a9
rnrn8407 <nisinisikame@yahoo.com>
2024-07-12 15:41:29 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict"
+{
+    const text=document.getElementById("text");
+    const button=document.getElementById("button");
+
+    button.addEventListener("click",()=>{
+        text.textContent="ばか";
+    });
+}
